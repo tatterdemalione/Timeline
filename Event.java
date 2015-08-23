aa321cb60e6a473e69efb19034b357c2ac56848c
Brennan <brennan@freemor.ca>
2015-08-23 09:50:06 -0400

    Create Event.java

---
+++ Event.java
@@ -0,0 +1,57 @@
+
+
+package timeline2;
+
+import java.awt.image.BufferedImage;
+
+public class Event {
+    private String year;
+    private String title;
+    private String info;
+    private BufferedImage img;
+    private int index;
+    
+    
+    public Event(String year, String title, String text, BufferedImage img, int index){
+        this.year=year;
+        this.title=title;
+        this.info=text;
+        this.img = img;
+        this.index = index;
+        
+        
+    }
+    
+    /* GET METHODS */
+    public String getYear(){
+       return year; 
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getInfo(){
+        return info;
+    }
+    public BufferedImage getImage(){
+        return img;
+    }
+    
+    public void setYear(String year){
+        this.year = year;
+    }
+    
+    public void setIndex(int index){
+        this.index = index;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public void setInfo(String info){
+        this.info=info;
+    }
+    public void setImage(BufferedImage img){
+        this.img = img;
+    }
+}
